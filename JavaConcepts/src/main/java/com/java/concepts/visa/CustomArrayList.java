4dd1f5420b3d67a89034a5af6d3032be26f494ec
Anuj Kumar <anujgupta1403@gmail.com>
2026-07-21 09:10:34 +0530

    custom arraylist

---
+++ JavaConcepts/src/main/java/com/java/concepts/visa/CustomArrayList.java
@@ -0,0 +1,37 @@
+package com.java.concepts.visa;
+
+public class CustomArrayList{
+    private int size;
+    private final Object[] array;
+    private int currentIndex = 0;
+
+    CustomArrayList(int size){
+        array= new Object[size];
+    }
+
+    public void add(Object item){
+        if(array[currentIndex]!=null){
+            currentIndex++;
+        }
+        array[currentIndex] = item;
+    }
+
+    public Object get(int index){
+        if(index<0) {
+            System.out.println("Invalid Index: " + index);
+            throw new IndexOutOfBoundsException();
+        }
+        if(array[index]!=null){
+                return (Object) array[index];
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        CustomArrayList customArrayList = new CustomArrayList(3);
+        customArrayList.add(10);
+        customArrayList.add(20);
+        customArrayList.add(30);
+        System.out.println(customArrayList.get(2));
+    }
+}
